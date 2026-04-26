13d081e7fd0f0bf172c52a25cf0bff85c2973598
wahyukw <wahyukwan@gmail.com>
2026-04-26 21:41:03 +0700

    Whitelist .PNG assets and use absolute base URL for SPA sub-routes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
+  assetsInclude: ['**/*.PNG'],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
